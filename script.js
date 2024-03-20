06006be63207cae989bf0df5becd0c1cce484eeb
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-03-19 19:16:05 -0700

    added querySelectorAll method

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// // Cache at least one element using selectElementById in JavaScript
+// // Cache at least one element using selectElementById
 const fullNameInput = document.getElementById('fullName');
 
 console.log(fullNameInput)
+// querySelectorAll method
+const allElements = document.querySelectorAll('.my-class');
+console.log(allElements);
